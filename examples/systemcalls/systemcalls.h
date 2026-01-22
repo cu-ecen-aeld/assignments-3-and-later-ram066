1dd2b086729376cd50ae1ba48b282e3e1860e86a
ram066 <rrkumar066@gmail.com>
2026-01-22 22:19:58 +0530

    Fix includes and implement systemcalls header file

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdlib.h>     // system(), exit(), EXIT_FAILURE
+#include <unistd.h>     // fork(), execv(), dup2(), STDOUT_FILENO
+#include <sys/wait.h>   // waitpid(), WIFEXITED, WEXITSTATUS
+#include <sys/types.h>  // pid_t
+#include <fcntl.h>      // open(), O_WRONLY, O_CREAT, O_TRUNC
+#include <stdio.h>      // fflush()
 
 bool do_system(const char *command);
 
